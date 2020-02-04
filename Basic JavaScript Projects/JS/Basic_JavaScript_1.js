15159bc06d020015bc24dc8d39cb22eac570b1de
Blake Berry <blakeberry85@yahoo.com>
2020-02-03 17:48:44 -0800

    Add Basic JavaScript

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,18 @@
+window.alert("Hello World!");
+document.write("Hello World! ");
+var A = "This is a string .";
+document.write(A);
+var B = "Hi there.";
+window.alert(B);
+document.write("I said \"Hello World!\" ");
+document.write("I visited somewhere amazing today,"
++ " it was breathtaking. ");
+var C = "Dogs" + " and" + " cats. ";
+document.write(C);
+var D = "Three", E = "Ten", F = "Twenty-two", G = "Four";
+var D = D.fontcolor("yellow");
+var E = E.fontcolor("red");
+var F = F.fontcolor("purple");
+var G = G.fontcolor("green");
+document.write(F);
+document.write(5+6);
