7c197d0e6ddab25dba68cab1abd8aa3de17cc271
Blake Berry <blakeberry85@yahoo.com>
2020-02-19 22:42:08 -0800

    Add Basic JavaScript 7

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -0,0 +1,52 @@
+X = 24;
+function add() {
+    Y = 25;
+    document.write(X + Y + "<br>");
+    console.log(X + Y);
+}
+
+function multiply() {
+    document.write(X * Y);
+    console.log(X * Y);
+}
+
+add();
+multiply();
+
+function greeting() {
+    if (new Date().getHours() < 12) {
+        document.getElementById("date").innerHTML = "Good morning!";
+    }
+}
+
+function quiz() {
+    color = document.getElementById("question").value;
+    if (color == "Blue") {
+        document.getElementById("result").innerHTML = "Correct";
+    }
+}
+
+function quiz_Alphabet() {
+    answer = document.getElementById("alphabet").value;
+    if (answer == 26) {
+        document.getElementById("response").innerHTML = "That's right!";
+    }
+    else {
+        document.getElementById("response").innerHTML = "Nope, sorry!";
+    }
+}
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "it is morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
